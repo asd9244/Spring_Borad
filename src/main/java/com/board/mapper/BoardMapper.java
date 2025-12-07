a22c9d2a81ec0b9937826f75dffb6b88e040d140
LeeSeungEun <jjong42004@gmail.com>
2025-12-07 16:59:03 +0900

    로그인 기능 구현 2일차

--- src/main/java/com/board/mapper/BoardMapper.java
+++ src/main/java/com/board/mapper/BoardMapper.java
@@ -19,7 +19,7 @@ public interface BoardMapper {
     // 게시글 내용을 **수정(Update)**하고, 작업 성공 시 영향을 받은 행의 수(1)를 반환합니다.
     public int updateBoard(BoardDTO params);
 
-    //    특정 idx에 해당하는 게시글을 삭제 처리하고, 작업 성공 시 영향을 받은 행의 수(1)를 반환합니다.
+    //  특정 idx에 해당하는 게시글을 삭제 처리하고, 작업 성공 시 영향을 받은 행의 수(1)를 반환합니다.
     public int deleteBoard(Long idx);
 
     // **검색 조건(Criteria)**에 맞는 게시글 목록을 조회합니다. 페이징 처리 정보(시작 위치, 개수)도 이 객체를 통해 전달받습니다.
--- src/main/java/com/board/mapper/MemberMapper.java
+++ src/main/java/com/board/mapper/MemberMapper.java
@@ -3,34 +3,41 @@ package com.board.mapper;
 import com.board.domain.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Date;
 import java.util.List;
 
 @Mapper
 public interface MemberMapper {
 
-    // 회원등록, Create하고 작업 성공 시 생성된 행의 수(1)를 반환한다.
+    // Create, Update, Delete 쿼리의 마이바티스 Return Type: int
+
+    // CREATE 회원등록, 작업 성공 시 생성된 행의 수(1)를 반환한다.
     public int insertMember(MemberDTO params);
 
-    // 회원정보, Update하고 작업 성공 시 변경된 행의 수(1)를 반환한다.
+    // UPDATE 회원정보, 작업 성공 시 변경된 행의 수(1)를 반환한다.
     public int updateMember(MemberDTO params);
 
-    //회원조회, 회원 1명의 상세정보를 조회한다.
-    public MemberDTO selectMemberDetail(MemberDTO memberId);
+    //SELECT 회원조회, 회원 1명의 상세정보를 조회한다.
+    public MemberDTO selectMemberDetail(String memberId);
 
-    // 전체회원조회, 회원 전체의 상세정보를 조회한다.
+    // SELECT 전체회원조회, 회원 전체의 상세정보를 조회한다.
     public List<MemberDTO> selectMemberList(MemberDTO params);
 
-    // 회원탈퇴, 회원의 status를 inactive로 변경한다. 작업 성공 시 행의 수(1)을 반환한다.
-    public int deleteMember(MemberDTO memberId);
+    // UPDATE 회원탈퇴, 회원의 status를 inactive로 변경한다. 작업 성공 시 행의 수(1)을 반환한다.
+    public int deleteMember(String memberId);
 
-    // 중복회원확인, 0보다 큰 수가 반환되면, 아이디 중복.
-    public int selectMemberIdCheck(MemberDTO memberId);
+    // SELECT 중복회원확인, 0보다 큰 수가 반환되면, 아이디 중복.
+    public int selectMemberIdCheck(String memberId);
 
-    //로그인기능, 아이디로 비밀번호를 조회해서, 일치하는지 확인. 일치하면 1 반환.
-    public MemberDTO selectMemberByID(MemberDTO params);
+    // SELECT 중복이메일확인, 0보다 큰 수가 반환되면, 아이디 중복.
+    public int selectEmailCheck(String email);
 
-    // 마지막접속시간, 로그인 할 때 접속시간 Update
-    public Date updateLastLogin(MemberDTO params);
+    // SELECT 계정상태조회, 로그인 할 때, status가 inactive면 로그인 중단.
+    public String selectMemberStatus(String memberId);
+
+    // SELECT 로그인기능, 아이디로 비밀번호를 조회해서, 일치하는지 확인. 일치하면 1 반환.
+    public MemberDTO selectMemberById(String memberId);
+
+    // UPDATE 마지막접속시간, 로그인 할 때 접속시간 Update
+    public int updateLastLogin(MemberDTO params);
 
 }
